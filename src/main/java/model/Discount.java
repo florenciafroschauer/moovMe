78d4fefddcbb2a1e313d94725a704be9a38c4217
AlexisPortocarrero <56561232+AlexisPortocarrero@users.noreply.github.com>
2019-10-14 19:32:20 -0300

    Creacion de "User" y "Zone"

---
+++ src/main/java/model/Discount.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Discount {
+    String assetType;
+    Integer minScore;
+    Zone zone;
+    int percent;
+}
---
+++ src/main/java/model/Zone.java
@@ -0,0 +1,6 @@
+package model;
+
+public class Zone {
+    String type;
+    Integer  rate;
+}
