ed15ad1ffcb8396f4a17b5feeb00eb46b6d2ffc2
BuildTools <unconfigured@null.spigotmc.org>
2019-10-14 20:08:24 -0300

    Diseño inicial de cliente que solo posee los atributos.

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,13 @@
+package model;
+import java.util.List;
+
+
+public class Client {
+
+    String phoneNumber;
+    String nickname;
+    Boolean state;
+    List<Zone> zones;
+    Integer score;
+    List<Discount> discounts;
+}
