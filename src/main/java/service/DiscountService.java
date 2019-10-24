6c5f8d95aa730ea4ff42fa7961bbfca92a068e9e
AlexisPortocarrero <56561232+AlexisPortocarrero@users.noreply.github.com>
2019-10-23 22:08:15 -0300

    creacion de DiscountService

--- src/main/java/service/DiscountService.java
+++ src/main/java/service/DiscountService.java
@@ -1,4 +1,25 @@
 package service;
 
+import model.Discount;
+import model.Zone;
+
 public class DiscountService {
+    private Discount discount;
+
+    public String showAssetType(Discount discount) {
+        return discount.getAssetType();
+    }
+
+    public Integer showMinScore(Discount discount) {
+        return discount.getMinScore();
+    }
+
+    public Zone showZone(Discount discount) {
+        return discount.getZone();
+    }
+
+    public int showPercent(Discount discount) {
+        return discount.getPercent();
+
+    }
 }
