5f7d9b794a6fc57ab1345864c0db15fee1fae5ed
AgustinAugurusa <agustin.augurusa@ing.edu.ar>
2019-10-23 21:37:46 -0300

    Client Service

--- src/main/java/service/ClientService.java
+++ src/main/java/service/ClientService.java
@@ -1,4 +1,20 @@
 package service;
 
+import model.Client;
+import model.Discount;
+
+import java.util.List;
+
 public class ClientService {
+
+    private Client client;
+
+    public Integer showScore(Client client) {
+        return client.getScore();
+    }
+
+    public List<Discount>showDiscounts(Client client) {
+        return client.showDiscounts();
+    }
+
 }
