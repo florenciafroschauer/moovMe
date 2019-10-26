6c5192c31c00a8465496eda7a5361def3f92969d
AlexisPortocarrero <56561232+AlexisPortocarrero@users.noreply.github.com>
2019-10-26 19:33:15 -0300

    Cambios de DiscountService

--- src/main/java/service/DiscountService.java
+++ src/main/java/service/DiscountService.java
@@ -1,25 +1,12 @@
 package service;
 
+import model.Client;
 import model.Discount;
-import model.Zone;
 
 public class DiscountService {
     private Discount discount;
 
-    public String showAssetType(Discount discount) {
-        return discount.getAssetType();
-    }
-
-    public Integer showMinScore(Discount discount) {
-        return discount.getMinScore();
-    }
-
-    public Zone showZone(Discount discount) {
-        return discount.getZone();
-    }
-
-    public int showPercent(Discount discount) {
-        return discount.getPercent();
-
+    public boolean canUse(Discount discount, Client client) {
+        return client.getScore() >= discount.getMinScore();
     }
 }
