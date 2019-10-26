d05eaebd4b77779aded5ba3784b252362bbf360d
AlexisPortocarrero <56561232+AlexisPortocarrero@users.noreply.github.com>
2019-10-26 20:24:31 -0300

    cambios a Discount.service

--- src/main/java/service/DiscountService.java
+++ src/main/java/service/DiscountService.java
@@ -7,7 +7,9 @@ import model.Trip;
 public class DiscountService {
     private Discount discount;
 
-    public boolean canUse(Discount discount, Client client, Trip trip) {
-        return client.getScore() >= discount.getMinScore() && trip.getAsset().getType().equals(discount.getAssetType());
+    public boolean canUse(Discount discount, Trip trip) {
+        return trip.getClient().getScore() >= discount.getMinScore() &&
+                trip.getAsset().getType().equals(discount.getAssetType()) &&
+                trip.getZone().getType().equals(discount.getZone().getType());
     }
 }
