63251ef8d26941d3f638a4cb00f268f0ed0f06a1
BuildTools <unconfigured@null.spigotmc.org>
2019-10-28 00:44:09 -0300

    Se agregó setTerminal

--- src/main/java/model/TripI.java
+++ src/main/java/model/TripI.java
@@ -6,6 +6,9 @@ public interface TripI {
     void setDiscount(Discount discount);
     void setToHandOver(Terminal toHandOver);
     void setTripTime(Date tripTime);
+    void setAsset(Asset asset);
+    void setZone(Zone zone);
+    void setTerminal(Terminal terminal);
     Client getClient();
     Asset getAsset();
     Zone getZone();
@@ -13,4 +16,5 @@ public interface TripI {
     Discount getDiscount();
     Terminal getTerminal();
     Tariff getTariff();
+
 }
