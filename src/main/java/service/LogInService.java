c7bb7926831d872176ccc36a4a20cffca9bb8be4
BuildTools <unconfigured@null.spigotmc.org>
2019-10-29 22:49:08 -0300

    Se creo de forma basica el servicio de login.

--- src/main/java/service/LogInService.java
+++ src/main/java/service/LogInService.java
@@ -1,6 +1,30 @@
 package service;
 
+import model.Client;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LogInService {
 
-    // tenemos que los usuarios entran con nickname y contraseña, pero los admin no tienen nickname
+    // tenemos que hacer que los usuarios entran con nickname y contraseña, pero los admin no tienen nickname
+    Boolean loginStatus;
+    public void logIn(String loginUsername, String loginPassword, Client client, ArrayList clients){
+
+
+        for(int i = 0; i < clients.size(); i++){
+
+            /** Falta aclarar la lista de clientes */
+
+            if(loginUsername.equals(clients(i).getUsername) && loginPassword.equals(clients(i).getPassword)){
+
+                loginStatus = true;
+                System.out.println("Successfuly logged in.");
+
+                break;
+
+            }
+
+        }
+    }
 }
