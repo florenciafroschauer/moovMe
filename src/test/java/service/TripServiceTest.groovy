af6f116fec8635f441a4f10b1f8c908ed1efcac4
BuildTools <unconfigured@null.spigotmc.org>
2019-11-04 22:00:19 -0300

    Se creó TripServiceTest

---
+++ src/test/java/service/TripServiceTest.groovy
@@ -0,0 +1,24 @@
+package service
+
+class TripServiceTest {
+    void testSetZone() {
+    }
+
+    void testSetAsset() {
+    }
+
+    void testSetTripTime() {
+    }
+
+    void testSetDiscount() {
+    }
+
+    void testSetTerminalToHandOver() {
+    }
+
+    void testSetTariff() {
+    }
+
+    void testSetScore() {
+    }
+}
