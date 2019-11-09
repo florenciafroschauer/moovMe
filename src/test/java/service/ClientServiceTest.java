2582694112299b1580f6c09dba1ceb492c8ee01e
AgustinAugurusa <agustin.augurusa@ing.edu.ar>
2019-11-08 22:32:56 -0300

    ClientServiceTest

--- src/test/java/service/ClientServiceTest.java
+++ src/test/java/service/ClientServiceTest.java
@@ -1,16 +1,56 @@
 package service;
 
+import model.Client;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ClientServiceTest {
-
+    public Client client = new Client("Test", "Test", "111111", "Test");
+    public ClientService clientService = new ClientService();
     @Test
     public void showScore() {
+        clientService.showScore(client);
+        assertEquals(client.getAccumulatedScore(), clientService.showScore(client));
     }
 
     @Test
     public void showDiscounts() {
+        clientService.showDiscounts(client);
+        assertEquals(client.getDiscounts(), clientService.showDiscounts(client));
     }
+    @Test
+
+    public void showVouchers(){
+        clientService.showDiscounts(client);
+        assertEquals(client.getVouchers(), clientService.showVouchers(client));
+    }
+    @Test
+    public void showPhoneNumber(){
+        clientService.showPhoneNumber(client);
+        assertEquals(client.getPhoneNumber(), clientService.showPhoneNumber(client));
+    }
+
+    @Test
+    public void showNickname(){
+        clientService.showNickname(client);
+        assertEquals(client.getNickname(), clientService.showNickname(client));
+    }
+
+    @Test
+    public void buyDiscounts(){//Hacerlo
+
+    }
+
+    @Test
+    public void useDiscounts(){//Hacerlo
+
+    }
+
+    @Test
+    public void useVoucher(){//Hacerlo
+
+    }
+
+
 }
