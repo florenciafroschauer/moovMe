02e8784654c6d7d0b4ca1069e08108f89ad0e5ae
AgustinAugurusa <agustin.augurusa@ing.edu.ar>
2019-11-11 02:46:52 -0300

    Repo

--- src/main/java/repository/DiscountRepository.java
+++ src/main/java/repository/DiscountRepository.java
@@ -10,7 +10,42 @@ public class DiscountRepository extends AbstractRepository<Discount> {
 
     private static DiscountRepository discountRepository;
 
-    private DiscountRepository() {}
+    private DiscountRepository() {
+        create(new Discount(new AssetType("Bici",
+                10, 15), 40,
+                new Zone("CABA", 10),
+                20));
+
+        create(new Discount(new AssetType("Moto",
+                5, 30), 50,
+                new Zone("Pilar", 15),
+                25));
+
+        create(new Discount(new AssetType("Scooter",
+                5, 20), 30,
+                new Zone("Vicente Lopez", 10),
+                10));
+
+        create(new Discount(new AssetType("Bici Moto",
+                20, 15), 30,
+                new Zone("Munro", 18),
+                5));
+
+        create(new Discount(new AssetType("MonoPatin",
+                13, 15), 80,
+                new Zone("Villa Crespo", 5),
+                35));
+
+        create(new Discount(new AssetType("Clio",
+                15, 15), 60,
+                new Zone("Pacheco", 10),
+                20));
+
+        create(new Discount(new AssetType("BatiMovil",
+                20, 15), 80,
+                new Zone("La lucila", 10),
+                30));
+    }
 
     public List<Discount> searchByAssetType(AssetType assetType) {
         List<Discount> discounts = findAll();
