d76352658fa23fda3d48030e9a64642b6599682f
AgustinAugurusa <agustin.augurusa@ing.edu.ar>
2019-11-11 02:54:47 -0300

    Repo3

--- src/main/java/repository/TripRepository.java
+++ src/main/java/repository/TripRepository.java
@@ -1,12 +1,17 @@
 package repository;
 
+import model.Client;
 import model.Trip;
 
 public class TripRepository extends AbstractRepository<Trip> {
 
     private static TripRepository tripRepository;
 
-    private TripRepository() {}
+    private TripRepository() {
+        create(new Trip(new Client("Agus", "123", 123, "Agus")));
+        create(new Trip(new Client("Flor", "1234", 1234, "Flor")));
+        create(new Trip(new Client("Santi", "1235", 12345, "Santi")));
+    }
 
     public static TripRepository getInstance() {
         if (tripRepository != null) {
